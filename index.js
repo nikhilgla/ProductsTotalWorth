e16e77dc0b7f8c9d36099a508f154c895d27b20b
nikhilgla <nikhil.baghel.32@gmail.com>
2023-08-09 22:37:02 +0530

    okkkkk

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ myForm.addEventListener('submit', onSubmit);
 var total = Number("0");
 
 window.addEventListener('DOMContentLoaded', () => {
-    axios.get('https://crudcrud.com/api/221dea5b3a494673870bc85f971d74e3/productsData')
+    axios.get('https://crudcrud.com/api/55426f4feb414de19ceaeb8bf1c1fe93/productsData')
         .then((ele) => {
             console.log(ele);
 
@@ -23,7 +23,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 })
 
-function onSubmit(e) {
+async function onSubmit(e) {
     e.preventDefault();
     console.log(nameInput.value);
 
@@ -33,7 +33,7 @@ function onSubmit(e) {
         amount: amount.value
     }
     
-        axios.post('https://crudcrud.com/api/221dea5b3a494673870bc85f971d74e3/productsData', myObj)
+        await axios.post('https://crudcrud.com/api/55426f4feb414de19ceaeb8bf1c1fe93/productsData', myObj)
             .then((ele) => {
                 console.log(ele.data);
                 showOnScreen(ele.data)
@@ -43,7 +43,7 @@ function onSubmit(e) {
     resetForm();
 }
 
-function showOnScreen(userObj) {
+async function showOnScreen(userObj) {
     this.total = this.total + Number(userObj.amount);
     console.log(this.total);
 
@@ -55,16 +55,16 @@ function showOnScreen(userObj) {
 
 }
 
-function deleteExp(name, id ,amount) {
+async function deleteExp(name, id ,amount) {
     const cc = document.getElementById(name);
     //var li = cc.parentElement;
     itemList.removeChild(cc);
     
-    this.total = this.total - Number(userObj.amount);
+    this.total = this.total - Number(amount);
     const childtotal = `<h3>Total Value worth of Products : ${this.total}</h3>`;
     tot.innerHTML = childtotal;
 
-    axios.delete(`https://crudcrud.com/api/221dea5b3a494673870bc85f971d74e3/productsData/${id}`)
+    await axios.delete(`https://crudcrud.com/api/55426f4feb414de19ceaeb8bf1c1fe93/productsData/${id}`)
         .then((ele) => { console.log(ele) })
         .catch((err) => { console.log(err); });
 }
